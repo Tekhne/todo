6e1e6200c30817a7ed890a555827a26d2abd1f53
Peter Havens <peter.havens@gmail.com>
2019-04-15 10:31:59 -0600

    Configure testing packages

--- client-web/src/setupTests.js
+++ client-web/src/setupTests.js
@@ -1,4 +1,7 @@
-import { configure } from 'enzyme';
+import 'jest-dom/extend-expect';
+import 'jest-enzyme';
+import 'react-testing-library/cleanup-after-each';
 import Adapter from 'enzyme-adapter-react-16';
+import { configure } from 'enzyme';
 
 configure({ adapter: new Adapter() });
