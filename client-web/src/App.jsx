ef3d2fbb093cde7820ca0df2bd4bd6c9369943c9
Peter Havens <peter.havens@gmail.com>
2019-04-15 10:46:02 -0600

    Simplify component

--- client-web/src/App.jsx
+++ client-web/src/App.jsx
@@ -1,10 +1,8 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.scss';
 
-export class App extends Component {
-  render() {
-    return <span>App</span>;
-  }
+export function App() {
+  return <span>App</span>;
 }
 
 export default App;
