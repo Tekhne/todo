6b0088abe3885d8490b7c53d08e69694b60f661a
Peter Havens <peter.havens@gmail.com>
2019-04-16 11:32:36 -0600

    Build initial content and styles for welcome page

--- client-web/src/Welcome.jsx
+++ client-web/src/Welcome.jsx
@@ -5,14 +5,67 @@ import Viewport from './Viewport';
 export function Welcome() {
   return (
     <Viewport>
-      <Content>
-        <section>
-          <h1>Todo Lists Improved</h1>
-          <p>
-            Combine a todo list with a day planner. It's simple, and it will
-            revolutionize your productivity.
-          </p>
-        </section>
+      <Content className="welcome">
+        <div className="hero">
+          <section className="hero-headers">
+            <h1 className="hero-header">
+              <span>Todo List</span>
+              <span><span className="hero-header-plus">+</span> Day Planner</span>
+            </h1>
+            <h2 className="hero-subheader">
+              It's simple. It works. It will transform your results.
+            </h2>
+          </section>
+          <section className="hero-form">
+            <form>
+              <div className="hero-form-field">
+                <div>
+                  <label htmlFor="username">Username</label>
+                </div>
+                <div className="hero-form-field-input">
+                  <input
+                    id="username"
+                    name="username"
+                    placeholder="smithuser"
+                    type="text"
+                    value=""
+                  />
+                </div>
+              </div>
+              <div className="hero-form-field">
+                <div>
+                  <label htmlFor="email">Email</label>
+                </div>
+                <div className="hero-form-field-input">
+                  <input
+                    id="email"
+                    inputMode="email"
+                    name="email"
+                    placeholder="smith@example.com"
+                    type="email"
+                    value=""
+                  />
+                </div>
+              </div>
+              <div className="hero-form-field">
+                <div>
+                  <label htmlFor="password">Password</label>
+                </div>
+                <div className="hero-form-field-input">
+                  <input
+                    id="password"
+                    name="password"
+                    type="password"
+                    value=""
+                  />
+                </div>
+              </div>
+              <div className="hero-form-button">
+                <button className="call-to-action" type="submit">Sign up for FREE</button>
+              </div>
+            </form>
+          </section>
+        </div>
       </Content>
     </Viewport>
   );
--- client-web/src/Welcome.mocha.jsx
+++ client-web/src/Welcome.mocha.jsx
@@ -1,4 +1,6 @@
+import Content from './Content';
 import React from 'react';
+import Viewport from './Viewport';
 import expect from 'expect';
 import { Welcome } from './Welcome';
 import { shallow } from 'enzyme';
@@ -14,5 +16,15 @@ function buildComponent(props = {}) {
 describe('Welcome', () => {
   it('renders successfully', () => {
     shallow(buildComponent());
+  });
+
+  it('renders Viewport', function() {
+    const component = shallow(buildComponent()).find(Viewport);
+    expect(component.exists).toBeTruthy();
+  });
+
+  it('renders Content', function() {
+    const component = shallow(buildComponent()).find(Content);
+    expect(component.exists).toBeTruthy();
   });
 });
