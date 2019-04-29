c6a2ce1ff7afdba420d88d4e30f0c3d575f21893
Peter Havens <peter.havens@gmail.com>
2019-04-29 11:57:49 -0600

    Improve test organization; re-format code

--- server/spec/models/username_credential_spec.rb
+++ server/spec/models/username_credential_spec.rb
@@ -7,15 +7,43 @@ RSpec.describe UsernameCredential, type: :model do
     expect(username_credential.defined_enums).to include('password_digest_type')
   end
 
-  describe 'validation' do
-    it { is_expected.to belong_to(:account) }
-    it { is_expected.to have_db_column(:account_id) }
-    it { is_expected.to have_db_column(:password_digest) }
-    it { is_expected.to have_db_column(:password_digest_type) }
-    it { is_expected.to have_db_column(:username) }
-    it { is_expected.to have_db_index(:username) }
-    it { is_expected.to have_secure_password }
+  context 'with a database' do
+    it do
+      expect(username_credential).to \
+        have_db_column(:account_id)
+        .of_type(:integer)
+        .with_options(foreign_key: true, null: false)
+    end
 
+    it do
+      expect(username_credential).to \
+        have_db_column(:password_digest)
+        .of_type(:string)
+        .with_options(null: false)
+    end
+
+    it do
+      expect(username_credential).to \
+        have_db_column(:password_digest_type)
+        .of_type(:enum)
+        .with_options(default: 'bcrypt', null: false)
+    end
+
+    it do
+      expect(username_credential).to \
+        have_db_column(:username)
+        .of_type(:string)
+        .with_options(null: false)
+    end
+
+    it { is_expected.to have_db_index(:username) }
+  end
+
+  context 'with assocations' do
+    it { is_expected.to belong_to(:account) }
+  end
+
+  context 'with validations' do
     it 'ensures password has a minimum length' do
       expect(username_credential).to \
         validate_length_of(:password)
@@ -32,12 +60,16 @@ RSpec.describe UsernameCredential, type: :model do
     end
   end
 
-  describe 'callbacks' do
+  it { is_expected.to have_secure_password }
+
+  context 'with callbacks' do
     describe '.before_validation' do
-      it 'normalizes Unicode of username' do
-        username_credential.username = "a\u0300"
-        username_credential.validate
-        expect(username_credential.username).to eq("\u00E0")
+      describe '#normalize_username' do
+        it 'normalizes Unicode of username' do
+          username_credential.username = "a\u0300"
+          username_credential.validate
+          expect(username_credential.username).to eq("\u00E0")
+        end
       end
     end
   end
