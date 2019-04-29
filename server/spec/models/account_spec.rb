a241c833b561096d0975397a41ce55aecd069e62
Peter Havens <peter.havens@gmail.com>
2019-04-29 12:06:14 -0600

    Improve tests; re-format code

--- server/spec/models/account_spec.rb
+++ server/spec/models/account_spec.rb
@@ -3,7 +3,18 @@ require 'rails_helper'
 RSpec.describe Account, type: :model do
   subject(:account) { described_class.new }
 
-  it { is_expected.to have_db_column(:status) }
-  it { is_expected.to have_db_index(:status) }
-  it { is_expected.to validate_presence_of(:status) }
+  context 'with a database' do
+    it do
+      expect(account).to \
+        have_db_column(:status)
+        .of_type(:enum)
+        .with_options(default: 'active', null: false)
+    end
+
+    it { is_expected.to have_db_index(:status) }
+  end
+
+  context 'with validations' do
+    it { is_expected.to validate_presence_of(:status) }
+  end
 end
