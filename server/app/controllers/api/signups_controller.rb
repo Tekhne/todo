e4f44b9d2ab208b1e908dd3021ed5e6d3008bc77
Peter Havens <peter.havens@gmail.com>
2019-05-09 09:36:19 -0600

    Only attempt signup process for JSON requests

--- server/app/controllers/api/signups_controller.rb
+++ server/app/controllers/api/signups_controller.rb
@@ -1,18 +1,24 @@
 class Api::SignupsController < ApplicationController
   def create
-    Signups.new.signup(create_params)
-    @message = I18n.t('signups_controller.create.success')
-    respond_to { |f| f.json { render status: :created } }
-  rescue Signups::ParamErrors => e
-    @message = I18n.t('application_controller.common.param_errors')
-    @param_errors = e.errors
-    respond_to { |f| f.json { render status: :unprocessable_entity } }
-  rescue Signups::ServiceError
-    @message = I18n.t('application_controller.common.service_error')
-    respond_to { |f| f.json { render status: :internal_server_error } }
-  rescue Signups::UnconfirmedEmail
-    @message = I18n.t('signups_controller.create.unconfirmed_email')
-    respond_to { |f| f.json { render status: :unprocessable_entity } }
+    respond_to do |format|
+      format.json do
+        begin
+          Signups.new.signup(create_params)
+          @message = I18n.t('signups_controller.create.success')
+          render :create, status: :created
+        rescue Signups::ParamErrors => e
+          @message = I18n.t('application_controller.common.param_errors')
+          @param_errors = e.errors
+          render :create, status: :unprocessable_entity
+        rescue Signups::ServiceError
+          @message = I18n.t('application_controller.common.service_error')
+          render :create, status: :internal_server_error
+        rescue Signups::UnconfirmedEmail
+          @message = I18n.t('signups_controller.create.unconfirmed_email')
+          render :create, status: :unprocessable_entity
+        end
+      end
+    end
   end
 
   private
