421637c712685fc4195ef08e783b8daacac9ea17
Peter Havens <peter.havens@gmail.com>
2019-05-09 12:27:03 -0600

    Change tests to use react-testing-library

--- client/src/Welcome.mocha.jsx
+++ client/src/Welcome.mocha.jsx
@@ -1,20 +1,45 @@
-import Content from './Content';
 import React from 'react';
-import Viewport from './Viewport';
-import expect from 'expect';
-import { Welcome } from './Welcome';
-import { shallow } from 'enzyme';
+import ReactDOM from 'react-dom';
+import Welcome from './Welcome';
+import { MemoryRouter } from 'react-router';
+import { ServicesContext } from './ServicesContext';
+import { cleanup, render } from 'react-testing-library';
+import { fake } from 'sinon';
 
-function buildProps(props = {}) {
-  return { ...props };
-}
+describe('Welcome', function() {
+  this.timeout(5000);
 
-function buildComponent(props = {}) {
-  return <Welcome {...buildProps(props)} />;
-}
+  function buildWrapper({ services }) {
+    return ({ children }) => (
+      <MemoryRouter>
+        <ServicesContext.Provider value={services}>
+          {children}
+        </ServicesContext.Provider>
+      </MemoryRouter>
+    );
+  }
 
-describe('Welcome', () => {
-  it('renders successfully', () => {
-    shallow(buildComponent());
+  function renderComponent(props = {}) {
+    const wrapper = buildWrapper({ services });
+    return render(<Welcome {...props} />, { wrapper });
+  }
+
+  let services;
+
+  beforeEach(function() {
+    services = { serverApi: { send: fake.resolves() } };
+  });
+
+  afterEach(cleanup);
+
+  it.only('renders successfully', function() {
+    const Wrapper = buildWrapper({ services });
+    const div = document.createElement('div');
+    ReactDOM.render(
+      <Wrapper>
+        <Welcome />
+      </Wrapper>,
+      div
+    );
   });
 });
