fb96ce2e315bfe9597a0b7708009a808b7b829bb
Peter Havens <peter.havens@gmail.com>
2019-05-14 07:59:11 -0600

    Add custom <title> for page-level components

--- client/src/Login.jsx
+++ client/src/Login.jsx
@@ -1,7 +1,15 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import { Helmet } from 'react-helmet';
 
 export function Login() {
-  return <span>Login</span>;
+  return (
+    <Fragment>
+      <Helmet>
+        <title>Login - App Name</title>
+      </Helmet>
+      <span>Login</span>
+    </Fragment>
+  );
 }
 
 export default Login;
--- client/src/Welcome.jsx
+++ client/src/Welcome.jsx
@@ -1,28 +1,34 @@
 import Content from './Content';
-import React from 'react';
+import React, { Fragment } from 'react';
 import SignupForm from './SignupForm';
 import Viewport from './Viewport';
+import { Helmet } from 'react-helmet';
 
 export function Welcome() {
   return (
-    <Viewport>
-      <Content className="welcome">
-        <div className="hero">
-          <section className="hero-headers">
-            <h1 className="hero-header">
-              <span>Todo List</span>{' '}
-              <span>
-                <span className="hero-header-plus">+</span> Day Planner
-              </span>
-            </h1>
-            <h2 className="hero-subheader">
-              It's simple. It works. Boost your productivity.
-            </h2>
-          </section>
-          <SignupForm />
-        </div>
-      </Content>
-    </Viewport>
+    <Fragment>
+      <Helmet>
+        <title>Welcome - App Name</title>
+      </Helmet>
+      <Viewport>
+        <Content className="welcome">
+          <div className="hero">
+            <section className="hero-headers">
+              <h1 className="hero-header">
+                <span>Todo List</span>{' '}
+                <span>
+                  <span className="hero-header-plus">+</span> Day Planner
+                </span>
+              </h1>
+              <h2 className="hero-subheader">
+                It's simple. It works. Boost your productivity.
+              </h2>
+            </section>
+            <SignupForm />
+          </div>
+        </Content>
+      </Viewport>
+    </Fragment>
   );
 }
 
