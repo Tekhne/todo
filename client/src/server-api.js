8ae6149f7c236982fad04e3cf6b22728206cdd96
Peter Havens <peter.havens@gmail.com>
2019-05-20 15:59:28 -0600

    Improve error handling

--- client/src/server-api.js
+++ client/src/server-api.js
@@ -1,11 +1,28 @@
 import axios from 'axios';
-import { has } from 'lodash';
+import { get, has, set } from 'lodash';
 
+/* istanbul ignore next */
 export const routes = {
+  login: () => '/api/login',
   signup: () => '/api/signup'
 };
 
+/* istanbul ignore next */
+function buildErrorMessage(response) {
+  switch (response.status) {
+    case 404:
+      return 'The location we sent your request to is missing.';
+    case 408:
+      return 'Your request timed out.';
+    case 429:
+      return 'The service is currently overloaded with requests.';
+    default:
+      return 'An unexpected error occurred.';
+  }
+}
+
 export class ServerApi {
+  /* istanbul ignore next */
   constructor({ ajax } = {}) {
     this.ajax = ajax || axios;
   }
@@ -17,18 +34,29 @@ export class ServerApi {
 
   async send({ data, method, route }) {
     if (!has(routes, route)) throw new Error(`bad route: ${route}`);
+    let response;
 
-    const response = await this.ajax({
-      data,
-      headers: {
-        'Content-Type': 'application/json',
-        Accept: 'application/json'
-      },
-      method,
-      url: routes[route]()
-    });
+    try {
+      response = await this.ajax({
+        data,
+        headers: {
+          'Content-Type': 'application/json',
+          Accept: 'application/json'
+        },
+        method,
+        url: routes[route]()
+      });
+    } catch (error) {
+      if (!error.response) throw error;
+      response = error.response;
+    }
 
-    if (response.status >= 400) throw response;
+    if (response.status >= 400) {
+      if (get(response, 'data.message')) throw response;
+      set(response, 'data.message', buildErrorMessage(response));
+      throw response;
+    }
+
     return response;
   }
 }
--- client/src/server-api.mocha.js
+++ client/src/server-api.mocha.js
@@ -5,13 +5,22 @@ import { assert, fake } from 'sinon';
 describe('ServerApi', function() {
   describe('send()', function() {
     const data = { test: 'test data' };
-    const response = { data: { message: 'test message' }, status: 200 };
     let ajax;
+    let response;
     let serverApi;
 
     beforeEach(function() {
+      response = { data: { message: 'test message' }, status: 200 };
       ajax = fake.returns(Promise.resolve(response));
       serverApi = new ServerApi({ ajax });
+    });
+
+    describe('when given route is invalid', function() {
+      it('rejects with an error', async function() {
+        await expect(
+          serverApi.send({ method: 'post', route: 'invalid' })
+        ).rejects.toThrow(Error);
+      });
     });
 
     it('sends request to server', async function() {
@@ -29,35 +38,56 @@ describe('ServerApi', function() {
       });
     });
 
+    describe('when server request fails', function() {
+      describe('when there is no server response', function() {
+        it('rejects with the error encountered', async function() {
+          const error = new Error('test error');
+          ajax = fake.returns(Promise.reject(error));
+          serverApi = new ServerApi({ ajax });
+          await expect(
+            serverApi.send({ data, method: 'post', route: 'signup' })
+          ).rejects.toEqual(error);
+        });
+      });
+
+      describe('when server response has status >= 400', function() {
+        describe('when response has message', function() {
+          it('rejects with response (as is)', async function() {
+            response.status = 404;
+            const error = new Error('test error');
+            error.response = response;
+            ajax = fake(() => Promise.reject(error));
+            serverApi = new ServerApi({ ajax });
+            await expect(
+              serverApi.send({ data, method: 'post', route: 'signup' })
+            ).rejects.toEqual(response);
+          });
+        });
+
+        describe('when response does not have message', function() {
+          it('rejects with response (with a new message)', async function() {
+            response.status = 404;
+            delete response.data.message;
+            const error = new Error('test error');
+            error.response = response;
+            ajax = fake(() => Promise.reject(error));
+            serverApi = new ServerApi({ ajax });
+            await expect(
+              serverApi.send({ data, method: 'post', route: 'signup' })
+            ).rejects.toMatchObject({
+              ...response,
+              data: { ...response.data, message: expect.any(String) }
+            });
+          });
+        });
+      });
+    });
+
     describe('when server request succeeds', function() {
       it('resolves with a response object', async function() {
-        const result = await serverApi.send({
-          data,
-          method: 'post',
-          route: 'signup'
-        });
-        expect(result).toEqual(response);
-      });
-    });
-
-    describe('when server request fails', function() {
-      it('resolves with an error response object', async function() {
-        const response = {
-          data: { message: 'test error message' },
-          status: 500
-        };
-        serverApi = new ServerApi({ ajax: fake.resolves(response) });
         await expect(
           serverApi.send({ data, method: 'post', route: 'signup' })
-        ).rejects.toEqual(response);
-      });
-    });
-
-    describe('when given route is invalid', function() {
-      it('rejects with an error', async function() {
-        await expect(
-          serverApi.send({ method: 'post', route: 'invalid' })
-        ).rejects.toThrow(Error);
+        ).resolves.toEqual(response);
       });
     });
   });
