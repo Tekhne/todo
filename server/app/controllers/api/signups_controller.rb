80b30fa62e22995c7e8f788fab0a7520415b7f6f
Peter Havens <peter.havens@gmail.com>
2019-06-01 09:13:04 -0600

    Format code

--- server/app/controllers/api/signups_controller.rb
+++ server/app/controllers/api/signups_controller.rb
@@ -2,21 +2,19 @@ class Api::SignupsController < ApplicationController
   def create
     respond_to do |format|
       format.json do
-        begin
-          Signups.new.signup(create_params)
-          @message = I18n.t('signups_controller.create.success')
-          render :create, status: :created
-        rescue Signups::ParamErrors => e
-          @message = I18n.t('application_controller.common.param_errors')
-          @param_errors = e.errors
-          render :create, status: :unprocessable_entity
-        rescue Signups::ServiceError
-          @message = I18n.t('application_controller.common.service_error')
-          render :create, status: :internal_server_error
-        rescue Signups::UnconfirmedEmail
-          @message = I18n.t('signups_controller.create.unconfirmed_email')
-          render :create, status: :unprocessable_entity
-        end
+        Signups.new.signup(create_params)
+        @message = I18n.t('signups_controller.create.success')
+        render :create, status: :created
+      rescue Signups::ParamErrors => e
+        @message = I18n.t('application_controller.common.param_errors')
+        @param_errors = e.errors
+        render :create, status: :unprocessable_entity
+      rescue Signups::ServiceError
+        @message = I18n.t('application_controller.common.service_error')
+        render :create, status: :internal_server_error
+      rescue Signups::UnconfirmedEmail
+        @message = I18n.t('signups_controller.create.unconfirmed_email')
+        render :create, status: :unprocessable_entity
       end
     end
   end
