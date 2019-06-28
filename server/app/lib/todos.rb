c1f7c9aece0e8b8385e91f68770a0badf39a7bfc
Peter Havens <peter.havens@gmail.com>
2019-06-28 13:35:52 -0600

    Set a valid manual priority for todo items

--- server/app/lib/todos.rb
+++ server/app/lib/todos.rb
@@ -3,7 +3,19 @@ class Todos
   include HandlesExceptions
 
   def create(account, params)
-    TodoItem.create!(account: account, description: params[:todo])
+    # FIXME avoid a manual_priority race condition in a performant way
+
+    old_maximum_manual_priority = TodoItem
+      .where(account: account)
+      .maximum(:manual_priority)
+
+    new_maximum_manual_priority = (old_maximum_manual_priority || 0) + 1
+
+    TodoItem.create!(
+      account: account,
+      description: params[:todo],
+      manual_priority: new_maximum_manual_priority
+    )
   rescue ActiveRecord::RecordInvalid => e
     unless e.record.errors[:description].present?
       log_exception e
