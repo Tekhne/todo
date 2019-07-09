91a99da45b4e526d27bba441cd784fcf870d8ab9
Peter Havens <peter.havens@gmail.com>
2019-07-09 15:10:30 -0600

    Update packages

--- client/src/mochaSetup.js
+++ client/src/mochaSetup.js
@@ -5,7 +5,7 @@ global.window.Date = Date;
 const expect = require('expect');
 global.expect = expect;
 global.window.expect = expect;
-require('jest-dom/extend-expect');
+require('@testing-library/jest-dom/extend-expect');
 
 // Prevent webpack imports from causing errors.
 require.extensions['.css'] = () => ({});
